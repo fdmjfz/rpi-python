9fe2438ad3740ccaf7a51b6e8d9ee5f8e102e166
fdmjfz <fejofefe@gmail.es>
2023-02-18 13:06:46 +0100

    Update

--- hc12_E.py
+++ hc12_E.py
@@ -1,19 +1,20 @@
-import time
+#!/usr/bin/env python
+import os
 import serial
+
+device_id = os.getenv('LOGNAME')
 
 ser = serial.Serial(
     port='/dev/ttyS0',
     baudrate = 9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=2)
-counter=0
+)
 
 try:
     while 1:
-        a = input("Mensaje: ")
-        ser.write( bytes( a, encoding='utf8')) 
+        message = input("Mensaje: ")
+        message = f'{device_id}~' + message
+        message = message + '>'
+        ser.write( bytes( message, encoding='utf8'))
 
 except KeyboardInterrupt:
     print("Exiting")
